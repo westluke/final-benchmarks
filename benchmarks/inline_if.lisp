891555f370a43da67c05956cc19d2329eb53a841
Rob Lewis <rob.y.lewis@gmail.com>
2022-12-04 17:36:10 -0500

    fix

--- benchmarks/inline_if.lisp
+++ benchmarks/inline_if.lisp
@@ -1,6 +1,6 @@
 (define (f x) (+ x 100))
 (define (proc x) (let ((y 10)) (+ y x)))
-(define (if_inline x) (if (= x 10) proc x))
+(define (if-inline x) (if (= x 10) (proc x) (f x)))
 
-(if_inline 10)
+(if-inline 10)
 
