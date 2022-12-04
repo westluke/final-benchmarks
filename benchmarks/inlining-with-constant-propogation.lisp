e5d101b690ec3cb139eb3b5cb264276453b09b68
Arvind Yalavarti <yalavartiarvind@gmail.com>
2022-12-04 17:46:32 -0500

    [ArYalavarti] Add benchmarks (#35)

---
+++ benchmarks/inlining-with-constant-propogation.lisp
@@ -0,0 +1,24 @@
+(define (f x) (let ((y 10)) (+ x y)))
+(define (g x) (let ((y 5)) (- x y)))
+(print (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g 1
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+)))))))))))))))))))))))))))
---
+++ benchmarks/multiple-inlining.lisp
@@ -0,0 +1,24 @@
+(define (f x) 10)
+(define (g x) x)
+(print (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g
+(f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g (f (g 1
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+))))))))))))))))))))))))))
+)))))))))))))))))))))))))))
